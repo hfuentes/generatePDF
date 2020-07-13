bd880ac96dd84a34c606c49f0d971f4e8bcde6c3
Hector Fuentes <hector.fuentes@imagemaker.com>
2020-07-13 01:23:44 -0400

    pdf changes added

--- src/index.js
+++ src/index.js
@@ -3,24 +3,90 @@ const PDFDocument = require('pdf-lib').PDFDocument
 const rgb = require('pdf-lib').rgb
 const StandardFonts = require('pdf-lib').StandardFonts
 const fs = require('fs')
+const moment = require('moment')
 
-async function main() {
+const date = {
+    year: 2020, month: 7, day: 13,
+    hour: 11, min: 21, seg: 55,
+    format: 'DD-MM-YYYY HH:mm:ss'
+}
+const data = {
+    broadcast: moment(new Date(
+        date.year,
+        date.month - 1,
+        date.day,
+        date.hour,
+        date.min - 3,
+        date.seg - 4
+    )).format(date.format),
+    until: moment(new Date(
+        date.year,
+        date.month - 1,
+        date.day,
+        date.hour + 3,
+        date.min,
+        date.seg
+    )).format(date.format),
+    from: moment(new Date(
+        date.year,
+        date.month - 1,
+        date.day,
+        date.hour,
+        date.min,
+        date.seg
+    )).format(date.format),
+    outputFile: 'output.pdf'
+}
+
+async function main(data) {
     const existingPdfBytes = fs.readFileSync('./src/templates/template_01.pdf')
     const pdfDoc = await PDFDocument.load(existingPdfBytes)
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
     const pages = pdfDoc.getPages()
     const firstPage = pages[0]
-    const { width, height } = firstPage.getSize()
-    firstPage.drawText('This text was added with JavaScript!', {
-        x: 5,
-        y: height / 2 + 300,
-        size: 50,
+    firstPage.drawRectangle({
+        x: 90,
+        y: 143,
+        width: 60,
+        height: 10,
+        color: rgb(1, 1, 1)
+    })
+    firstPage.drawText(data.broadcast, {
+        x: 90,
+        y: 145,
+        size: 6,
+        font: helveticaFont
+    })
+    firstPage.drawRectangle({
+        x: 85,
+        y: 235,
+        width: 70,
+        height: 10,
+        color: rgb(1, 1, 1)
+    })
+    firstPage.drawText(data.until, {
+        x: 88.5,
+        y: 237.5,
+        size: 6.7,
         font: helveticaFont,
-        color: rgb(0.95, 0.1, 0.1),
-        rotate: degrees(-45),
+        color: rgb(0, 0.45, 0.16)
+    })
+    firstPage.drawRectangle({
+        x: 85,
+        y: 248,
+        width: 70,
+        height: 10,
+        color: rgb(1, 1, 1)
+    })
+    firstPage.drawText(data.from, {
+        x: 88.5,
+        y: 251,
+        size: 6.7,
+        font: helveticaFont,
+        color: rgb(0, 0.45, 0.16)
     })
     const pdfBytes = await pdfDoc.save()
-    fs.writeFileSync('./src/templates/output.pdf', pdfBytes)
+    fs.writeFileSync('./src/templates/' + data.outputFile, pdfBytes)
 }
 
-main()
+main(data)
